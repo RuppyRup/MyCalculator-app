370d00e3d20f73c1d4e1b2546daa7c7c9efa7fe0
RuppyRup <rupert.waldron@yahoo.co.uk>
2017-02-10 16:50:22 +0000

    Cal app all buttons connected and code appears to work.

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -38,23 +38,58 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didPressPlus(_ sender: Any) {
+        changeMode(newMode: .addition)
     }
 
     @IBAction func didPressSubtract(_ sender: Any) {
+        changeMode(newMode: .subtraction)
     }
     
     @IBAction func didPressMultiply(_ sender: Any) {
+        changeMode(newMode: .multiplication)
     }
     
     @IBAction func didPressEquals(_ sender: Any) {
+        guard let labelInt:Int = Int(labelString) else {
+            return // does not update if 0 is pressed
+        }
+        if (currentMode == .not_set || lastButtonWasMode) {
+            return
+        }
+        if (currentMode == .addition) {
+            savedNum += labelInt
+        }
+        else if (currentMode == .subtraction) {
+            savedNum -= labelInt
+        }
+        else if (currentMode == .multiplication) {
+            savedNum = savedNum * labelInt
+        }
+        currentMode = .not_set
+        labelString = "\(savedNum)"
+        updateText()
+        lastButtonWasMode = true
     }
     
     @IBAction func didPressClear(_ sender: Any) {
+        
+        labelString = "0"
+        currentMode = .not_set
+        savedNum = 0
+        lastButtonWasMode = false
+        label.text = "0"
+        
     }
     
     
     @IBAction func didPressNumber(_ sender: UIButton) {
         let stringValue:String? = sender.titleLabel?.text
+        
+        if (lastButtonWasMode) {
+            lastButtonWasMode = false
+            labelString = "0"
+        }
+        
         labelString = labelString.appending(stringValue!)
         updateText()
     }
@@ -67,11 +102,18 @@ class ViewController: UIViewController {
         guard let labelInt:Int = Int(labelString) else {
             return // does not update if 0 is pressed
         }
+        if (currentMode == .not_set) {
+            savedNum = labelInt
+        }
         label.text = "\(labelInt)"
     }
     
     func changeMode(newMode:modes) {
-        
+        if (savedNum == 0) {
+            return
+        }
+        currentMode = newMode
+        lastButtonWasMode = true
     }
     
     
